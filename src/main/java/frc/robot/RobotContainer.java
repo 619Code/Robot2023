61bff830c9b6d6e9cc18305cdcf43d1a9c178898
Straw17 <strawmcstick@gmail.com>
2023-01-10 18:53:31 -0500

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+
+public class RobotContainer {
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+
+  }
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
