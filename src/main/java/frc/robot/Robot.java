08723fbabb9c52f6b164ced21a2225621228869a
JingerJesus <jingerjesus420@gmail.com>
2023-01-12 19:02:59 -0500

    Basic drivetrain stuff CHANGE CONSTANT VALS

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,11 +6,11 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  private RobotContainer m_robotContainer;
+  private RobotContainer robotContainer;
 
   @Override
   public void robotInit() {
-    m_robotContainer = new RobotContainer();
+    robotContainer = new RobotContainer();
   }
 
   @Override
@@ -26,7 +26,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,93 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+
+    DifferentialDrive drive;
+    MotorControllerGroup leftMotors;
+    MotorControllerGroup rightMotors;
+    CANSparkMax leftLeader;
+    CANSparkMax rightLeader;
+
+    RelativeEncoder leftEncoder;
+    RelativeEncoder rightEncoder;
+
+    AHRS navx;
+    DifferentialDriveKinematics kinematics; // converts rotation and velocity to wheel velocities
+    DifferentialDriveOdometry odometry; // tracks robot position on the field
+
+    public Drivetrain() {
+        leftLeader = new CANSparkMax(Constants.LEFT_LEADER, MotorType.kBrushless);
+        CANSparkMax leftMotorArray[] = {
+            leftLeader,
+            new CANSparkMax(Constants.LEFT_FOLLOWER_0, MotorType.kBrushless),
+            new CANSparkMax(Constants.LEFT_FOLLOWER_1, MotorType.kBrushless)
+        }; 
+        for(CANSparkMax spark : leftMotorArray) {
+            spark.setIdleMode(IdleMode.kCoast);
+            //spark.setSmartCurrentLimit(45);
+        }
+        leftLeader.setIdleMode(IdleMode.kBrake); //experimental
+
+        rightLeader = new CANSparkMax(Constants.RIGHT_LEADER, MotorType.kBrushless);
+        CANSparkMax rightMotorArray[] = {
+            rightLeader,
+            new CANSparkMax(Constants.RIGHT_FOLLOWER_0, MotorType.kBrushless),
+            new CANSparkMax(Constants.RIGHT_FOLLOWER_1, MotorType.kBrushless)
+        };
+        for(CANSparkMax spark : rightMotorArray) {
+            spark.setIdleMode(IdleMode.kCoast);
+            //spark.setSmartCurrentLimit(45);
+        }
+        rightLeader.setIdleMode(IdleMode.kBrake); //experimental
+        
+        leftMotors = new MotorControllerGroup(leftMotorArray);
+        rightMotors = new MotorControllerGroup(rightMotorArray);
+        
+        // invert right motor
+        rightMotors.setInverted(true);
+
+        // encoders
+        leftEncoder = leftLeader.getEncoder();
+        rightEncoder = rightLeader.getEncoder();
+        leftEncoder.setVelocityConversionFactor(Constants.RPM_TO_VELOCITY_CONVERSION_FACTOR);
+        rightEncoder.setVelocityConversionFactor(Constants.RPM_TO_VELOCITY_CONVERSION_FACTOR);
+
+        // drive
+        drive = new DifferentialDrive(leftMotors, rightMotors);
+        drive.setSafetyEnabled(false);
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        double leftVelocity = (leftEncoder.getVelocity() * Constants.DRIVE_RATIO_HIGH) * (Constants.WHEEL_DIAMETER * Math.PI) * (1/60);
+        double rightVelocity = (rightEncoder.getVelocity() * Constants.DRIVE_RATIO_HIGH) * (Constants.WHEEL_DIAMETER * Math.PI) * (1/60);
+        return new DifferentialDriveWheelSpeeds(leftVelocity, rightVelocity);
+    }
+
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        leftMotors.setVoltage(leftVolts);
+        rightMotors.setVoltage(rightVolts);
+        //System.out.println("Left volts: " + leftVolts);
+        //System.out.println("Right volts: "+ rightVolts);
+        drive.feed();
+    }
+
+    public void curve(double speed, double rotation, boolean isLowGear) {
+        drive.curvatureDrive(Constants.SPEED_ADJUST * speed, Constants.SPEED_ADJUST * -rotation, true);
+    }
+
+}
