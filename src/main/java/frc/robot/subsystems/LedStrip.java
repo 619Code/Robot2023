1840ec7ac427a656d73d665a49b90b1a4b4a9d03
Peter Edwards <peter.d.edwards@gmail.com>
2023-03-24 23:53:35 -0400

    working led code

--- src/main/java/frc/robot/subsystems/LedStrip.java
+++ src/main/java/frc/robot/subsystems/LedStrip.java
@@ -3,92 +3,64 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.States;
 import frc.robot.helpers.LedStripQuarter;
 import frc.robot.helpers.enums.LEDQuarterTypes;
 
 public class LedStrip extends SubsystemBase{
+
+    int rIndex = 0;
+    int gIndex = 1;
+    int bIndex = 2;
+
     AddressableLED ledStrip;
     AddressableLEDBuffer ledBuffer;
     int m_rainbowFirstPixelHue = 0;
-
-    int oneFourth, oneHalf, threeQuarts, full;
-
-    LedStripQuarter q1, q2, q3, q4;
+    //int oneFourth, oneHalf, threeQuarts, full;
+    LedStripQuarter[] ledSections = new LedStripQuarter[3];
+    //, q4;
 
     public boolean isYellow;
 
     public LedStrip() {
         ledStrip = new AddressableLED(0);
-        ledBuffer = new AddressableLEDBuffer(120);
+        ledBuffer = new AddressableLEDBuffer(280);
         ledStrip.setLength(ledBuffer.getLength());
         ledStrip.setData(ledBuffer);
         ledStrip.start();
 
-        oneFourth = ledBuffer.getLength()/4;
-        oneHalf = 2*ledBuffer.getLength()/4;
-        threeQuarts = 3*ledBuffer.getLength()/4;
-        full = ledBuffer.getLength();
-
-        //rainbow();
         ledStrip.setData(ledBuffer);
-        System.out.println("PZDFKGHESKJGNUI");
 
-        q1 = new LedStripQuarter(LEDQuarterTypes.KnightRider, ledBuffer.getLength()/4);
-        q2 = new LedStripQuarter(LEDQuarterTypes.Communication, ledBuffer.getLength()/4);
-        q3 = new LedStripQuarter(LEDQuarterTypes.Communication, ledBuffer.getLength()/4);
-        q4 = new LedStripQuarter(LEDQuarterTypes.KnightRider, ledBuffer.getLength()/4);
+        ledSections[0] = new LedStripQuarter(LEDQuarterTypes.Communication, 76);
+        ledSections[1] = new LedStripQuarter(LEDQuarterTypes.KnightRider, 94);
+        ledSections[2] = new LedStripQuarter(LEDQuarterTypes.KnightRider, 99);
 
         isYellow = true;
     }
 
     public void periodic() {
-        States.lightsAreYellow = isYellow;
-        
         //rainbow();
         readFromStrips();
-        // setWholeStripRGB(0, 0, 255, 0, ledBuffer.getLength()/4);
-        // setWholeStripRGB(255, 0, 255, ledBuffer.getLength()/4, 2 * ledBuffer.getLength()/4);
-        // setWholeStripRGB(0, 255, 0, ledBuffer.getLength()/2, 3*ledBuffer.getLength()/4);
-        // setWholeStripRGB(255, 0, 255, 3*ledBuffer.getLength()/4,ledBuffer.getLength());
-
         ledStrip.setData(ledBuffer);
     }
 
     private void readFromStrips() {
-
-        int[][] q1v = q1.getValues();
-        int[][] q2v = q2.getValues();
-        int[][] q3v = q3.getValues();
-        int[][] q4v = q4.getValues();
-
-        //kr stuff
-
-        for (int i = 0; i < oneFourth; i ++) {
-            ledBuffer.setRGB(i, q1v[i][0], q1v[i][1], q1v[i][2]);
+        int startIndex = 0;
+        for (int i = 0; i < ledSections.length; i++)
+        {
+            var values = ledSections[i].getValues();
+            setRange(startIndex, values);
+            startIndex = startIndex + values.length;
         }
+    }
 
-        for (int i = 0; i < oneFourth; i ++) {
-            ledBuffer.setRGB(i + oneFourth, q2v[i][0], q2v[i][1], q2v[i][2]);
+    private void setRange(int startIndex, int[][] values) {
+        for (int i = 0; i < values.length; i++) {
+            ledBuffer.setRGB(i + startIndex, values[i][rIndex], values[i][gIndex], values[i][bIndex]);
         }
-
-        for (int i = 0; i < oneFourth; i ++) {
-            ledBuffer.setRGB(i+oneHalf, q3v[i][0], q3v[i][1], q3v[i][2]);
-        }
-
-        for (int i = 0; i < oneFourth; i ++) {
-            ledBuffer.setRGB(i+threeQuarts, q4v[i][0], q4v[i][1], q4v[i][2]);
-        }
-        
     }
 
     private void setWholeStripRGB(int red, int green, int blue) {
         for (int i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, red, green, blue);
-        }
-    }
-    private void setPartialStripRGB(int red, int green, int blue, int start, int end) {
-        for (int i = start; i < end; i++) {
             ledBuffer.setRGB(i, red, green, blue);
         }
     }
