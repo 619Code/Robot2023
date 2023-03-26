a6d240faa222c1da7b76a29936d7f394488b8f19
Peter Edwards <peter.d.edwards@gmail.com>
2023-03-26 08:25:42 -0400

    Peter's LED Changes

--- src/main/java/frc/robot/helpers/LedStripQuarter.java
+++ src/main/java/frc/robot/helpers/LedStripQuarter.java
@@ -1,5 +1,6 @@
 package frc.robot.helpers;
 
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.States;
 import frc.robot.helpers.enums.LEDQuarterTypes;
 
@@ -7,7 +8,14 @@ public class LedStripQuarter {
     private int[][] ledRGBs;
     private LEDQuarterTypes type;
     private nightrider nr;
-    
+
+    private SimpleColor Orange = new SimpleColor(255, 64, 0);
+    private SimpleColor Blue = new SimpleColor(0, 0, 255);
+    private SimpleColor Yellow = new SimpleColor(255, 128,0);
+    private SimpleColor LightYellow = new SimpleColor(255, 179, 102);
+    private SimpleColor Purple = new SimpleColor(168, 0, 149);
+    private SimpleColor LightPurple = new SimpleColor(255, 102, 237);
+
     public LedStripQuarter(LEDQuarterTypes type, int length) {
         this.type = type;
         ledRGBs = new int[length][3]; //2D array, first array is LED array, second dimension is HSV values. Dont forget :3 
@@ -15,32 +23,61 @@ public class LedStripQuarter {
     }
 
     public int[][] getValues() {
-        if (type.equals(LEDQuarterTypes.Communication)) {
-            for (int i = 0; i < ledRGBs.length; i ++) {
-                if (States.lightsAreYellow) {
-                    ledRGBs[i][0] = 255;
-                    ledRGBs[i][1] = 128;
-                    ledRGBs[i][2] = 0; //V value reduced to darken LEDs and lower power draw.
-                } else {
-                    ledRGBs[i][0] = 168;
-                    ledRGBs[i][1] = 0;
-                    ledRGBs[i][2] = 149; //V value reduced to darken LEDs and lower power draw.
-                }
-            } return ledRGBs;
-        } else { //is KnightRider
-            boolean[] vals = nr.looooop();
-            for (int i = 0; i < vals.length; i ++) {
-                if (vals[i]) {
-                    ledRGBs[i][0] = 255;
-                    ledRGBs[i][1] = 64;
-                    ledRGBs[i][2] = 0; //V value reduced to darken LEDs and lower power draw.
-                } else {
-                    ledRGBs[i][0] = 0;
-                    ledRGBs[i][1] = 0;
-                    ledRGBs[i][2] = 255; //V value reduced to darken LEDs and lower power draw.
-                }
-            }
-            return ledRGBs;
+        switch(type)
+        {
+            case Communication:
+                return getValuesComms();
+            case KnightRiderComs: 
+                return getValuesKnightRiderComms();
+            default: 
+                return getValuesKnightRider();
         }
     }
+
+    private int[][] getValuesComms() {
+        for (int i = 0; i < ledRGBs.length; i ++) {
+            if (States.lightsAreYellow) {
+                ledRGBs[i][0] = Yellow.red;
+                ledRGBs[i][1] = Yellow.green;
+                ledRGBs[i][2] = Yellow.blue; //V value reduced to darken LEDs and lower power draw.
+            } else {
+                ledRGBs[i][0] = Purple.red;
+                ledRGBs[i][1] = Purple.green;
+                ledRGBs[i][2] = Purple.blue; //V value reduced to darken LEDs and lower power draw.
+            }
+        } return ledRGBs;
+    }
+
+    public int[][] getValuesKnightRiderComms() {
+        if (States.lightsAreYellow)
+        {
+            return this.getValuesKnightRider(LightYellow, Yellow);            
+        }
+        else
+        {
+            return this.getValuesKnightRider(LightPurple, Purple);
+        }
+    }
+
+    private int[][] getValuesKnightRider(SimpleColor inner, SimpleColor outer)
+    {
+        boolean[] vals = nr.looooop();
+        for (int i = 0; i < vals.length; i ++) {
+            if (vals[i]) {
+                ledRGBs[i][0] = inner.red;
+                ledRGBs[i][1] = inner.green;
+                ledRGBs[i][2] = inner.blue; //V value reduced to darken LEDs and lower power draw.
+            } else {
+                ledRGBs[i][0] = outer.red;
+                ledRGBs[i][1] = outer.green;
+                ledRGBs[i][2] = outer.blue; //V value reduced to darken LEDs and lower power draw.
+            }
+        }
+        return ledRGBs;
+    }
+
+    public int[][] getValuesKnightRider() {
+        return getValuesKnightRider(Orange, Blue);
+    }
+    
 }
---
+++ src/main/java/frc/robot/helpers/SimpleColor.java
@@ -0,0 +1,15 @@
+package frc.robot.helpers;
+
+public class SimpleColor {
+    public int red;
+    public int green;
+    public int blue;
+
+    public SimpleColor(int r, int g, int b)
+    {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+
+}
